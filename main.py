2db8a689efbaa676773d03dc75bcc4116216f1c7
henriits <121551949+henriits@users.noreply.github.com>
2023-02-25 12:21:44 +0200

    student method updated

--- main.py
+++ main.py
@@ -112,17 +112,19 @@ class EditDialog(QDialog):
 
         layout = QVBoxLayout()
 
-
         # Get student name from selected row
         index = student_manager.table.currentRow()
         student_name = student_manager.table.item(index, 1).text()
+        # Get id from selected row
+        self.student_id = student_manager.table.item(index, 0).text()
+
         # Add student name widget
         self.student_name = QLineEdit(student_name)
         self.student_name.setPlaceholderText("Name")
         layout.addWidget(self.student_name)
 
         # Add combo box of courses
-        course_name = student_manager.table.item(index,2).text()
+        course_name = student_manager.table.item(index, 2).text()
         self.course_name = QComboBox()
         courses = ["Biology", "Math", "Astronomy", "Physics"]
         self.course_name.addItems(courses)
@@ -130,7 +132,7 @@ class EditDialog(QDialog):
         layout.addWidget(self.course_name)
 
         # Add mobile widget
-        mobile = student_manager.table.item(index,3).text()
+        mobile = student_manager.table.item(index, 3).text()
         self.mobile = QLineEdit(mobile)
         self.mobile.setPlaceholderText("Mobile")
 
@@ -138,16 +140,25 @@ class EditDialog(QDialog):
 
         # Add a submit button
         button = QPushButton("Submit")
-        button.clicked.connect(self.update)
+        button.clicked.connect(self.update_student)
         layout.addWidget(button)
 
         self.setLayout(layout)
 
-
     def update_student(self):
         connection = sqlite3.connect("database.db")
         cursor = connection.cursor()
-        cursor.execute("UPDATE students SET name = ?, course = ?, mobile = ? WHERE id = ?",())
+        cursor.execute("UPDATE students SET name = ?, course = ?, mobile = ? WHERE id = ?",
+                       (self.student_name.text(),
+                        self.course_name.itemText(self.course_name.currentIndex()),
+                        self.mobile.text(), self.student_id))
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        # Refresh the table
+        student_manager.load_data()
 
 
 class InsertDialog(QDialog):
@@ -217,7 +228,7 @@ class SearchDialog(QDialog):
 
     def search(self):
         name = self.student_name.text()
-        connection = sqlite3.connect("../../Users/henri/Downloads/Solution (2)/database.db")
+        connection = sqlite3.connect("database.db")
         cursor = connection.cursor()
         result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
         row = list(result)[0]
